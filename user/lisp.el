a9a407459435de600605e6fb94e15c4cd9f33493
Ting-Yu Lin <aethanyc@gmail.com>
2012-08-12 21:31:13 +0800

    Remove useless emacs lisp hook

--- user/lisp.el
+++ user/lisp.el
@@ -6,7 +6,6 @@
 (add-hook 'emacs-lisp-mode-hook
           (lambda ()
             (local-set-key (kbd "C-c v") 'eval-buffer)))
-(add-hook 'emacs-lisp-mode-hook 'whitespace-cleanup)
 
 (define-key lisp-mode-shared-map (kbd "RET") 'reindent-then-newline-and-indent)
 
