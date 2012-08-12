5ce0c92a0abefef82c185478b445c68cb80b0707
Ting-Yu Lin <aethanyc@gmail.com>
2012-08-12 15:47:49 +0800

    Set linum-format and ediff settings

--- user/display.el
+++ user/display.el
@@ -11,7 +11,13 @@
       (set-frame-font "Pragmata-12" nil t))
 
   ;; Set frame title.
-  (setq-default frame-title-format '("%b" (buffer-file-name ": %f"))))
+  (setq-default frame-title-format '("%b" (buffer-file-name ": %f")))
+
+  ;; Set frame properties.
+  (add-to-list 'initial-frame-alist '(top . 0) t)
+  (add-to-list 'initial-frame-alist '(left . 0) t)
+  (add-to-list 'default-frame-alist '(width . 90) t)
+  (add-to-list 'default-frame-alist '(height . 45) t))
 
 (setq-default inhibit-startup-screen t
               visible-bell t)
@@ -27,14 +33,16 @@
 
 ;; Display line numbers in the left margin.
 (global-linum-mode 1)
+(setq-default linum-format "%5d|")
 
 ;; Display line and column number in the status bar.
 (column-number-mode 1)
 
+;; Turn on regexp highlighting.
 (global-hi-lock-mode 1)
 
-;; Set frame properties.
-(add-to-list 'initial-frame-alist '(top . 0) t)
-(add-to-list 'initial-frame-alist '(left . 0) t)
-(add-to-list 'default-frame-alist '(width . 90) t)
-(add-to-list 'default-frame-alist '(height . 45) t)
+;; Let ediff split window horizontally rather than vertically.
+(setq ediff-split-window-function 'split-window-horizontally)
+
+;; Put ediff control panel in single frame.
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
