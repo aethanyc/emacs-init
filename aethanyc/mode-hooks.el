28f0549e70929e64586dd6ca9324bf7fd54d2db4
Ting-Yu Lin <aethanyc@gmail.com>
2012-10-18 20:41:58 +0800

    Revise C++ mode hook

--- aethanyc/mode-hooks.el
+++ aethanyc/mode-hooks.el
@@ -51,18 +51,32 @@
 (defun my-c-mode-common-hook ()
   (semantic-mode 1)
   (electric-pair-mode 1)
-  (subword-mode 1)
-  (c-set-style "stroustrup")
+  (subword-mode 1)f
+  ;; (c-set-style "stroustrup")
   ;; C-c C-o: Current indentation level
-  (c-set-offset 'cpp-macro 0)
-  (c-set-offset 'topmost-intro-cont 4)
-  (c-set-offset 'arglist-intro 0)
-  (c-set-offset 'arglist-close 0)
-  (c-set-offset 'member-init-intro 0)
-  (c-set-offset 'inline-open 0)
+  ;; (c-set-offset 'cpp-macro 0)
+  ;; (c-set-offset 'topmost-intro-cont 4)
+  ;; (c-set-offset 'arglist-intro 0)
+  ;; (c-set-offset 'arglist-close 0)
+  ;; (c-set-offset 'member-init-intro 0)
+  ;; (c-set-offset 'inline-open 0)
   (local-set-key (kbd "RET") 'c-context-line-break))
 
 (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+
+(defconst my-c++-style
+  '("stroustrup"
+    (c-offsets-alist
+     (topmost-intro-cont . +)
+     (arglist-intro . 0)
+     (arglist-close . 0)
+     (member-init-intro . 0)
+     (cpp-macro . 0))))
+
+(defun my-c++-mode-hook ()
+  (c-add-style "my-c++-style" my-c++-style t))
+
+(add-hook 'c++-mode-hook 'my-c++-mode-hook)
 
 ;;;-------------------------------------------------------------------
 ;;; Text Mode
