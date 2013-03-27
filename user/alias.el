432eabf62f24b346a20b816a53f5ee60c5c96503
Ting-Yu Lin <aethanyc@gmail.com>
2013-03-27 19:02:05 +0800

    Add an alias `gl' for goto-line

--- user/alias.el
+++ user/alias.el
@@ -13,6 +13,7 @@
 (defalias 'ack-find-file 'ack-and-a-half-find-file)
 (defalias 'ack-find-file-same 'ack-and-a-half-find-file-same)
 
+(defalias 'gl 'goto-line)
 (defalias 'lp 'list-packages)
 (defalias 'mf 'maximize-frame)
 (defalias 'rf 'restore-frame)
