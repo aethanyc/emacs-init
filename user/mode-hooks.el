a51f68d4fb5d09ab23ffc6477afec3c0bb264000
Ting-Yu Lin <aethanyc@gmail.com>
2013-05-18 17:17:32 +0800

    Use python3 as the python shell interpreter
    Leave the ipython settings in the comment.

--- user/mode-hooks.el
+++ user/mode-hooks.el
@@ -82,7 +82,18 @@
 ;;; Python Mode
 
 (defun my-python-mode-hook ()
-  (semantic-mode 1))
+  ;; Ipython settings was copied from the document of python-mode.
+  ;; (setq python-shell-interpreter "ipython"
+  ;;       python-shell-interpreter-args ""
+  ;;       python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+  ;;       python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+  ;;       python-shell-completion-setup-code
+  ;;       "from IPython.core.completerlib import module_completion"
+  ;;       python-shell-completion-module-string-code
+  ;;       "';'.join(module_completion('''%s'''))\n"
+  ;;       python-shell-completion-string-code
+  ;;       "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+  (setq python-shell-interpreter "python3"))
 
 (add-hook 'python-mode-hook 'my-python-mode-hook)
 
