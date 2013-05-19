44dd6c62e2730b7b7f899abfcd476689bf3cbc6b
Ting-Yu Lin <aethanyc@gmail.com>
2013-05-19 19:46:04 +0800

    Use ipython3 or python3 only if it is installed
    To make the configuration portable, only set python-shell-interpreter if
    the external commmand is installed.

--- user/mode-hooks.el
+++ user/mode-hooks.el
@@ -82,18 +82,20 @@
 ;;; Python Mode
 
 (defun my-python-mode-hook ()
-  ;; Ipython settings was copied from the document of python-mode.
-  ;; (setq python-shell-interpreter "ipython"
-  ;;       python-shell-interpreter-args ""
-  ;;       python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-  ;;       python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-  ;;       python-shell-completion-setup-code
-  ;;       "from IPython.core.completerlib import module_completion"
-  ;;       python-shell-completion-module-string-code
-  ;;       "';'.join(module_completion('''%s'''))\n"
-  ;;       python-shell-completion-string-code
-  ;;       "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
-  (setq python-shell-interpreter "python3"))
+  (cond ((executable-find "ipython3")
+         ;; Ipython settings was copied from the document of python-mode.
+         (setq python-shell-interpreter "ipython3"
+               python-shell-interpreter-args ""
+               python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+               python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+               python-shell-completion-setup-code
+               "from IPython.core.completerlib import module_completion"
+               python-shell-completion-module-string-code
+               "';'.join(module_completion('''%s'''))\n"
+               python-shell-completion-string-code
+               "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
+        ((executable-find "python3")
+         (setq python-shell-interpreter "python3"))))
 
 (add-hook 'python-mode-hook 'my-python-mode-hook)
 
