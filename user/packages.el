0e8c94d7bd2b36b4398abb84b311546a3f1fd4c8
Ting-Yu Lin <aethanyc@gmail.com>
2013-05-27 23:23:48 +0800

    Correct initialization sequence of package

--- user/packages.el
+++ user/packages.el
@@ -36,10 +36,10 @@
                         )
   "A list of packages to ensure are installed at launch.")
 
-
-(package-initialize)
+(require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
 
 (when (not package-archive-contents)
   (package-refresh-contents))
