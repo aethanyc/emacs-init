082c5ad6e5576ba85a55a9ae55cb1c3fac156354
Ting-Yu Lin <aethanyc@gmail.com>
2013-11-01 22:37:37 +0800

    Fix navigation back after jumping by ace-jump-mode
    Replace `push-mark` with `back-button-push-mark-local-and-global` so
    that all the commands that call `push-mark` to preserve the current
    location can also keep a history in `back-button` mode. Later we can
    jump back by `back-button-global-backward`.

--- modules/aethanyc-editor.el
+++ modules/aethanyc-editor.el
@@ -258,6 +258,8 @@
 (use-package back-button
   :init
   (progn
+    (defalias 'push-mark 'back-button-push-mark-local-and-global
+      "Replace push-mark to preserve current position before jumping around.")
     (bind-key* "C-M-b" 'back-button-global-backward)
     (bind-key* "C-M-f" 'back-button-global-forward)
     (back-button-mode 1))
