cf3cefc42e2242f614582304a82a3edd3ceb024c
Ting-Yu Lin <aethanyc@gmail.com>
2014-04-08 15:34:31 +0800

    Fix #1: Do not show empty lines in whitespace-mode
    It doesn't look good with auto-complete.

--- modules/aethanyc-ui.el
+++ modules/aethanyc-ui.el
@@ -117,7 +117,7 @@ The value should be an alist of elements (FONT . CHARSET).")
 (use-package whitespace
   :init
   (progn
-    (setq-default whitespace-style '(face trailing tab-mark empty))
+    (setq-default whitespace-style '(face trailing tab-mark))
 
     ;; Turn on whitespace-mode only in file buffers.
     (add-hook 'find-file-hook 'whitespace-mode)))
