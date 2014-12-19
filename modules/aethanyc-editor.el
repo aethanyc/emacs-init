1bee64c145d180b2bb57fd45c98a740310d36c8a
Ting-Yu Lin <aethanyc@gmail.com>
2014-12-19 16:45:27 +0800

    Bump the priority of partial file name completion

--- modules/aethanyc-editor.el
+++ modules/aethanyc-editor.el
@@ -39,14 +39,14 @@
 
 ;; Adjust the behavior of the hippie-expand
 (setq hippie-expand-try-functions-list
-      '(try-complete-file-name
+      '(try-complete-file-name-partially
+        try-complete-file-name
         try-expand-all-abbrevs
         try-expand-dabbrev
         try-expand-dabbrev-all-buffers
         try-expand-dabbrev-from-kill
         try-complete-lisp-symbol-partially
         try-complete-lisp-symbol
-        try-complete-file-name-partially
         try-expand-list
         try-expand-line))
 
