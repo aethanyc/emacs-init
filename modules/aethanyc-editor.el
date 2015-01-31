8909c99595c14801b0e8912ec4c968e02ce94a12
Ting-Yu Lin <aethanyc@gmail.com>
2015-01-31 14:01:28 +0800

    Remove `ack-and-a-half' since it was abandoned
    Reference:
    https://github.com/bbatsov/projectile/issues/584

--- modules/aethanyc-editor.el
+++ modules/aethanyc-editor.el
@@ -232,16 +232,6 @@
   :ensure ace-jump-mode)
 
 
-(use-package ack-and-a-half
-  :init
-  (progn
-    (defalias 'ack 'ack-and-a-half)
-    (defalias 'ack-same 'ack-and-a-half-same)
-    (defalias 'ack-find-file 'ack-and-a-half-find-file)
-    (defalias 'ack-find-file-same 'ack-and-a-half-find-file-same))
-  :ensure ack-and-a-half)
-
-
 (use-package ag
   :ensure ag)
 
