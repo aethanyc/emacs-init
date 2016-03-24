d92a8dae17b019148fa1c3da607494cff2750579
Ting-Yu Lin <aethanyc@gmail.com>
2016-03-24 22:38:05 +0800

    Revert "Fix which-function-mode appear in mode line"
    This reverts commit f43f49aa7439b800f7919a8ad1f0b8fd13215cf0. The
    emacs-25 branch of Emacs does not need this fix.

--- modules/aethanyc-programming.el
+++ modules/aethanyc-programming.el
@@ -46,7 +46,7 @@
   (setq mode-line-misc-info
         ;; Remove Which Function Mode from the mode line, because it's mostly
         ;; invisible here anyway.
-        (assq-delete-all 'which-function-mode mode-line-misc-info))
+        (assq-delete-all 'which-func-mode mode-line-misc-info))
 
   (defun which-function-mode-setup ()
     (which-function-mode 1)
