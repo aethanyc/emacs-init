7b31ba165d96a55b10715bbff4f164520ce689ed
Ting-Yu Lin <aethanyc@gmail.com>
2018-09-10 10:28:36 -0700

    Fix unnecessary call to ‘package-initialize’ warning on Linux

--- init.el
+++ init.el
@@ -18,7 +18,8 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+(when (eq system-type 'darwin)
+  (package-initialize))
 
 (defvar aethanyc-modules-dir (locate-user-emacs-file "modules/")
   "This folder contains all the customization files.")
