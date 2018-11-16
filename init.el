c67609d6012e9fbe5c219c4e039c529edca3f2ba
Ting-Yu Lin <aethanyc@gmail.com>
2018-11-16 14:01:38 -0800

    Revert "Fix unnecessary call to ‘package-initialize’ warning on Linux"
    This reverts commit 7b31ba165d96a55b10715bbff4f164520ce689ed.
    
    (package-initialize) is needed on Emacs 26 branch.

--- init.el
+++ init.el
@@ -18,8 +18,7 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(when (eq system-type 'darwin)
-  (package-initialize))
+(package-initialize)
 
 (defvar aethanyc-modules-dir (locate-user-emacs-file "modules/")
   "This folder contains all the customization files.")
